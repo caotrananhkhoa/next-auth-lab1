92aa2e1f0b03da061210544116a13da114c4d05a
caotrananhk <caokhoa7a7@gmail.com>
2024-01-14 16:51:24 +0700

    Setup middleware

---
+++ middleware.ts
@@ -0,0 +1,14 @@
+import { auth } from "./auth"
+
+export default auth((req) => {
+  const isLoggedIn = !!req.auth
+  console.log("ROUTE: ", req.nextUrl.pathname)
+  console.log("IS LOGGEDIN? ", isLoggedIn)
+})
+
+// Matcher defines paths will invoke Middleware
+// The regular expression indicates all paths in this App.
+// To identify what is/are public routes, define in auth() definition
+export const config = {
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+}
