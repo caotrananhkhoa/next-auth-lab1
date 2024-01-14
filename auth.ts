b081d04e2743a8ed2a29565daf21ed58e486c959
caotrananhk <caokhoa7a7@gmail.com>
2024-01-14 23:18:22 +0700

    Add credentials provider

--- auth.ts
+++ auth.ts
@@ -1,5 +1,4 @@
 import NextAuth from "next-auth";
-import GitHub from "next-auth/providers/github";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 
 import authConfig from "@/auth.config";
@@ -8,6 +7,8 @@ import { db } from "@/lib/db";
 export const {
   handlers: { GET, POST },
   auth,
+  signIn,
+  signOut
 } = NextAuth({
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
