bc1e6b398e639696dfa5bab5d95d0e84a6de7d84
caotrananhk <caokhoa7a7@gmail.com>
2024-01-14 15:34:17 +0700

    Implement register Server action

--- actions/register.ts
+++ actions/register.ts
@@ -3,6 +3,9 @@
 import * as z from "zod"
 import { RegisterSchema } from "@/schemas"
 
+import bcrypt from "bcrypt"
+import { db } from "@/lib/db"
+
 export type RegisterStatusType = {
     status: number,
     message: string
@@ -10,14 +13,44 @@ export type RegisterStatusType = {
 export const register = async (value: z.infer<typeof RegisterSchema>) => {
     const validatedFields = RegisterSchema.safeParse(value)
 
+    // Validate all fields
     if (!validatedFields.success) {
         return {
             status: 1,
             message: "Invalid fields!"
         }
     }
+
+    // Get data
+    const { email, password, name } = validatedFields.data
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    // Exist user in database
+    const existingUser = await db.user.findUnique({
+        where: {
+            email
+        }
+    })
+
+    if (existingUser) {
+        return {
+            status: 1,
+            message: "Email already in used!"
+        }
+    }
+
+    await db.user.create({
+        data: {
+            name,
+            email,
+            password: hashedPassword
+        }
+    })
+
+    // TODO: Send verification token email
+
     return {
         status: 0,
-        message: "Email sent!"
+        message: "User created!"
     }
 }
