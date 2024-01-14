df88d94849a5d1966d33c269b71706ac054ac601
caotrananhk <caokhoa7a7@gmail.com>
2024-01-14 23:08:35 +0700

    Define publicRoute and config middleware

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,45 @@
-import NextAuth from "next-auth"
-import authConfig from "@/auth.config"
+import NextAuth from "next-auth";
+import authConfig from "@/auth.config";
+import {
+  DEFAULT_LOGIN_REDIRECT,
+  publicRoutes,
+  apiAuthPrefix,
+  authRoutes,
+} from "@/routes";
 
-const { auth } = NextAuth(authConfig)
+const { auth } = NextAuth(authConfig);
 
 export default auth((req) => {
-  const isLoggedIn = !!req.auth
-  console.log("ROUTE: ", req.nextUrl.pathname)
-  console.log("IS LOGGEDIN? ", isLoggedIn)
-})
+  const { nextUrl } = req;
+  const isLoggedIn = !!req.auth;
+
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+
+  if (isApiAuthRoute) {
+    return null;
+  }
+
+  // Redirect to DEFAULT_LOGIN_REDIRECT if logged in
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+    }
+    return null;
+  }
+
+  // Redirect to /auth/login if haven't logged in and isn't public route
+  if (!isLoggedIn && !isPublicRoute) {
+    return Response.redirect(new URL("/auth/login", nextUrl));
+  }
+
+  return null;
+});
 
 // Matcher defines paths will invoke Middleware
 // The regular expression indicates all paths in this App.
 // To identify what is/are public routes, define in auth() definition
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
-}
+};
