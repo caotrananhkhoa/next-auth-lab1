4331c2e8b472c03a505bebc0c7dc2ea9216d3726
caotrananhk <caokhoa7a7@gmail.com>
2024-01-17 00:49:36 +0700

    Setup callbacks temporary

--- auth.ts
+++ auth.ts
@@ -10,6 +10,27 @@ export const {
   signIn,
   signOut
 } = NextAuth({
+  callbacks: {
+    // Modify session
+    async session({ token, session }) {
+      // Create an id field, which get from token, in user object
+      // token.sub is id of current user
+      if (token.sub && session.user) {
+        session.user.id = token.sub
+      }
+      return session
+    },
+
+    // Create and/or modify token
+    async jwt({ token }) {
+      // Custom field (Ex: Access token from external server)
+      // const res = await fetch('https://dummyjson.com/products/1')
+      // const res2 = await res.json()
+      // token.customField = res2.title
+
+      return token
+    }
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,
