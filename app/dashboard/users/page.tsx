ff16997f448e1065a2c7c295353845f2e90c0c31
caotrananhk <caokhoa7a7@gmail.com>
2024-01-17 00:02:05 +0700

    Update to next chapter

---
+++ app/dashboard/users/page.tsx
@@ -0,0 +1,9 @@
+import RegisterForm from "@/components/auth/register-form"
+
+const UserPage = () => {
+    return (
+        <h1>This is user page</h1>
+    )
+}
+
+export default UserPage
--- auth.config.ts
+++ auth.config.ts
@@ -27,6 +27,7 @@ export default {
 
           if (passwordsMatch) return user;
         }
+        
         return null;
       },
     }),
